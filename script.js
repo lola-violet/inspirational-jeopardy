f939b41a66ad3f321b6f506d05ce9eef889fcf2a
JaHa675 <james.harding675@gmail.com>
2022-04-08 15:52:20 -0700

    question answer quote and author display on page

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
 function getJservice() {
-    var requestUrl = "http://jservice.io/api/random";
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data[0].question);
-        console.log("===================");
-        console.log(data[0].answer);
-      });
-  }
-  
-  
-  
-  function getQuote() {
-    var requestUrl = "https://type.fit/api/quotes";
-    fetch(requestUrl)
-    .then(function(response) {
+  var requestUrl = "http://jservice.io/api/random";
+
+  fetch(requestUrl)
+    .then(function (response) {
       return response.json();
     })
-    .then(function(data) {
-      var randomNumber = Math.floor(Math.random()*1643)
-      console.log(data[randomNumber].text);
-      console.log("===================");
-      console.log(data[randomNumber].author);
+    .then(function (data) {
+      const questionText = document.createElement("h2");
+      const answerText = document.createElement("h4");
+
+      questionText.textContent = data[0].question;
+      answerText.textContent = data[0].answer;
+
+      document.body.appendChild(questionText);
+      document.body.appendChild(answerText);
     });
-  }
-  
-  
-  getJservice();
-  getQuote();
+}
+
+function getQuote() {
+  var requestUrl = "https://type.fit/api/quotes";
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var randomNumber = Math.floor(Math.random() * 1643);
+
+      const quoteText = document.createElement("h2");
+      const authorText = document.createElement("h4");
+
+      quoteText.textContent = data[randomNumber].text;
+      authorText.textContent = data[randomNumber].author;
+
+      document.body.appendChild(quoteText);
+      document.body.appendChild(authorText);
+    });
+}
+
+getJservice();
+
+getQuote();
