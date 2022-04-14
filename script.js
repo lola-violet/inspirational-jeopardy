36d48cfe64c47983bd829bff207b0b84defb6d6c
Dahlia Graves <dahlialolagraves@gmail.com>
2022-04-14 11:50:50 -0700

    final page part 2 and localstorage

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ var value;
 var score = 0;
 
 
+
 // TODO: EVERYTHING RELATING TO PARAMETERS
 // TODO: generate 5 questions for each category
 // TODO: link boxes to the questions
@@ -119,6 +120,13 @@ var x=0;
 // function for dealing with questions as they get answered
 submitBtn.addEventListener("click", function () {
  x++;
+ if (x===15){
+  console.log("all answered");
+  scorePage();
+  hideGrid();
+  hideQuiz();
+  return
+}
   if (userAnswer.value.toLowerCase() == answerText.toLowerCase()) {
     value = parseInt(value);
     score = score + value;
@@ -135,10 +143,6 @@ submitBtn.addEventListener("click", function () {
     getQuote();
     showQuote();
   } console.log($(".question-row").children());
-  if (x===15){
-    console.log("all answered");
-    scorePage();
-  }
 });
 
 $("#moreQuotes").on("click", function(){
@@ -152,50 +156,6 @@ $("#nextQuestion").on("click", function(){
 
 function wrongAnswer() {
   getQuote();
-}
-
-// function that shows the quote
-function showQuote() {
-  quoteContainer.classList.remove("hide");
-}
-
-// function that hides shows the quote
-function hideQuote() {
-  quoteContainer.classList.add("hide");
-}
-
-// function that hides shows the quiz
-function showQuiz() {
-  questionContainer.classList.remove("hide");
-}
-
-// function that hides shows the quiz
-function showGrid() {
-  questionGrid.classList.remove("hide");
-}
-
-// function that hides shows the quiz
-function hideGrid() {
-  questionGrid.classList.add("hide");
-}
-
-// function that hides the quiz
-function hideQuiz() {
-  questionContainer.classList.add("hide");
-}
-// function that shows resultspage 
-function showFinal(){
-  $("#resultpage").removeClass("hide");
-}
-
-function removeTags(str) {
-  if (str === null || str === "") return false;
-  else str = str.toString();
-
-  // Regular expression to identify HTML tags in
-  // the input string. Replacing the identified
-  // HTML tag with a null string.
-  return str.replace(/(<([^>]+)>)/gi, "");
 }
 
 $(".dropdown-trigger").dropdown();
@@ -300,8 +260,69 @@ $("#category3")
       });
     }
   });
-
+  var pastScores = [];
   function scorePage () {
     showFinal();
-    $("#finalize").textContent = "Final Score is $" + score + "!";
+    $("#finalize").text("Final Score is $" + score + "!");
+    localStorage.setItem("score",JSON.stringify(score));
+    pastScores.push(score);
+    console.log(pastScores);
+    getScore();
   }
+  function getScore(){
+    var storedScores = JSON.parse(localStorage.getItem("pastScores"));
+    if (storedScores){
+      for(i=0;i<storedScores.length;i++){
+        var pastScoresList = $("#pastScoresList");
+        var pastScoresListItem = document.createElement("li");
+        pastScoresListItem.textContent = storedScores[i];
+        pastScoresList.prepend(pastScoresListItem);
+        pastScores.push(storedScores[i]);
+      }
+    };
+    localStorage.setItem("pastScores",JSON.stringify(pastScores));
+  }
+// function that shows the quote
+function showQuote() {
+  quoteContainer.classList.remove("hide");
+}
+
+// function that hides shows the quote
+function hideQuote() {
+  quoteContainer.classList.add("hide");
+}
+
+// function that hides shows the quiz
+function showQuiz() {
+  questionContainer.classList.remove("hide");
+}
+
+// function that hides shows the quiz
+function showGrid() {
+  questionGrid.classList.remove("hide");
+}
+
+// function that hides shows the quiz
+function hideGrid() {
+  questionGrid.classList.add("hide");
+}
+
+// function that hides the quiz
+function hideQuiz() {
+  questionContainer.classList.add("hide");
+}
+// function that shows resultspage 
+function showFinal(){
+  $("#resultpage").removeClass("hide");
+}
+
+function removeTags(str) {
+  if (str === null || str === "") return false;
+  else str = str.toString();
+
+  // Regular expression to identify HTML tags in
+  // the input string. Replacing the identified
+  // HTML tag with a null string.
+  return str.replace(/(<([^>]+)>)/gi, "");
+}
+
